60f7fc60a913ba9a67c8d6c67a9653f245a0b4c0
viktarb <120518178+viktarb@users.noreply.github.com>
2025-10-29 20:14:15 +0100

    Add initial JavaScript code with styling and logs

---
+++ Script1.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+const styles = `
+background: linear-gradient(#884ced, #ecicce);
+color:#fff;
+padding: 5px 10px;
+`;
+
+console.log("%c¡Hola manz!", styles)
+console.log("Hola Mundo");
+console.log(2 + 2);
